2d5c1413acf8691703c59a7610743cb08b78ac15
Hunter Ducharme <hgducharme@gmail.com>
2022-07-17 14:24:03 -0500

    reorganized

---
+++ 010_LongestPalindromicSubstring/main.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include "Solution.hpp"
+
+int main() {
+    Solution solution;
+    std::string testCase = "abba";
+
+    solution.longestPalindrome(testCase);
+
+    return 0;
+}
