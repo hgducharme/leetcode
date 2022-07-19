50b0eea7cc38fba8fc03d8e6a8b0bf9615c51176
Hunter Ducharme <hgducharme@gmail.com>
2022-07-18 23:36:22 -0500

    renamed 010 to 005

--- 005_LongestPalindromicSubstring/Solution.hpp
+++ 005_LongestPalindromicSubstring/Solution.hpp
@@ -8,8 +8,7 @@ class Solution {
 public:
     int longestPalindromeLength = 0;
     string palindrome = "";
-
-    void main();
+    
     string longestPalindrome(string str);
     string searchForPalindrome(string str, int left, int right);
     void updateResults(string str);
