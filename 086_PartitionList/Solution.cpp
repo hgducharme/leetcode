869117b086a868f10b804df443b6f20a3551a4c1
Hunter Ducharme <hgducharme@gmail.com>
2022-07-22 18:17:17 -0500

    Added 086

---
+++ 086_PartitionList/Solution.cpp
@@ -0,0 +1,7 @@
+#include "Solution.hpp"
+
+ListNode* Solution::partition(ListNode* head, int x) {
+    ListNode* ln;
+
+    return ln;
+}
---
+++ 086_PartitionList/main.cpp
@@ -0,0 +1,10 @@
+// #include <vector>
+#include "Solution.hpp"
+
+int main() {
+    Solution solution;
+
+    solution.partition(testCase);
+
+    return 0;
+}
