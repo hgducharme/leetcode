d1748c2500c8393261b1d461f849f58f677b3084
Hunter Ducharme <hgducharme@gmail.com>
2022-07-25 22:06:25 -0500

    WIP

--- 088_MergeSortedArray/Solution.cpp
+++ 088_MergeSortedArray/Solution.cpp
@@ -10,27 +10,34 @@ void Solution::merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, in
         return;
     }
 
-    int j = 0;
+    int j, k = 0;
     int placeFirst, placeLater;
+    int cachedNum = std::numeric_limits<int>::max();
+    std::vector<int> cache;
 
     for (int i = 0; i < nums1.size(); i++) {
         int num1 = nums1[i];
         int num2 = nums2[j];
+        if (!cache.empty()) {
+            cachedNum = cache[k];
+        }
+        
+        int numToInsert = std::min(num2, cachedNum);
 
         if (i >= n) {
-            nums1[i] = nums2[j];
-            j++;
+            nums1[i] = numToInsert;
         }
-        else if (num1 > num2) {
-            placeFirst = num2;
+        else if (num1 > numToInsert) {
             placeLater = num1;
-
-            nums1[i] = placeFirst;
-            nums2[j] = placeLater;
+            nums1[i] = numToInsert;
+            cache.push_back(placeLater);
         }
         else {
             // If num1 <= num2, just leave num1 alone and continue to the next element.
             // We only insert num2 into nums1 array if num2 > num1.
         }
+
+        if (numToInsert == num2) j++;
+        if (numToInsert == cachedNum) k++;
     }
 }
--- 088_MergeSortedArray/main.cpp
+++ 088_MergeSortedArray/main.cpp
@@ -2,21 +2,11 @@
 
 int main() {
     Solution solution;
-    int testCase = 10;
 
-    // std::vector<int> nums1{1,2,3,0,0,0};
-    // int m = 3;
-    // std::vector<int> nums2{2,5,6};
-    // int n = 3;
-
-// [4,5,6,0,0,0]
-// 3
-// [1,2,3]
-// 3
-    std::vector<int> nums1;
-    int m = 0;
-    std::vector<int> nums2{1};
-    int n = 1;
+    std::vector<int> nums1{4,5,6,0,0,0};
+    int m = 3;
+    std::vector<int> nums2{1,2,3};
+    int n = 3;
 
     solution.merge(nums1, m, nums2, n);
 
