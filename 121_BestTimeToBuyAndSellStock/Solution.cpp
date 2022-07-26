43e9753f2fbc015a368f0ad8546e229d1539c70a
Hunter Ducharme <hgducharme@gmail.com>
2022-07-26 16:29:37 -0500

    Finished 121

--- 121_BestTimeToBuyAndSellStock/Solution.cpp
+++ 121_BestTimeToBuyAndSellStock/Solution.cpp
@@ -1,5 +1,35 @@
 #include "Solution.hpp"
 
 int Solution::maxProfit(std::vector<int>& prices)  {
-    
+    int largestProfit = 0;
+    int runningProfit = std::numeric_limits<int>::min();
+    int boughtPrice = prices[0];
+
+    for (int i = 1; i < prices.size(); i++) {
+        int currentPrice = prices[i];
+        int currentProfit = currentPrice - boughtPrice;
+
+        if (currentProfit >= 0) {
+            if (currentProfit <= currentPrice) {
+                runningProfit = currentProfit;
+            }
+            else {
+                boughtPrice = currentPrice;
+                runningProfit = 0;
+            }
+        }
+        else {
+            if (currentProfit > currentPrice) {
+                runningProfit = currentProfit;
+            }
+            else {
+                boughtPrice = currentPrice;
+                runningProfit = 0;
+            }
+        }
+        
+        largestProfit = std::max(runningProfit, largestProfit);
+    }
+
+    return largestProfit;
 }
--- 121_BestTimeToBuyAndSellStock/main.cpp
+++ 121_BestTimeToBuyAndSellStock/main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     Solution solution;
-    std::vector<int> prices;
+    std::vector<int> prices{2,1,2,1,0,1,2};
 
     solution.maxProfit(prices) ;
 
